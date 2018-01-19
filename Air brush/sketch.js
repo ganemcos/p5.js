e188e995840ab2079aa1e281353de3b54e98d7ce
ganemcos <ganemcos@gmail.com>
2018-01-19 21:47:01 +0530

    Added air brush
    general paint utility

---
+++ Air brush/sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+	createCanvas(windowWidth, 700);
+	background(250,250,100);
+}
+
+function draw() {
+	noStroke();
+	fill(250,200,200,60);
+	ellipse(mouseX,mouseY,30,30);
+}
+
+function mousePressed()
+{
+	background(250,250,100);
+}
